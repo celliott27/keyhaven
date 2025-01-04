4ba5d25191716961d7dc6b8eed64ddef8e26f6a9
Chase Elliott <chasefelliott@gmail.com>
2025-01-04 18:33:09 -0500

    fix(tailwind): attempt to resolve build error caused by plugin - Initial fix was applied but build still fails. - Further investigation is required.

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import Home from "./page";
+import {it, expect, describe} from "vitest"
+
 
 describe("Home", () => {
   it("should show hello", () => {
     render(<Home />);
-    expect(screen.getByText(/hello/i)).toBeInTheDocument();
+    expect(screen.getByText(/dashboard/i)).toBeInTheDocument();
   });
 });
